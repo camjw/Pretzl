db8261e684b578ba97ebccb817725a9151866d08
Andres Faraone <andresfaraone@outlook.com>
2018-10-16 17:31:13 +0100

    basic test for hasBeenEaten

--- boulangerie/tests/bagel.test.js
+++ boulangerie/tests/bagel.test.js
@@ -2,26 +2,17 @@ const { bite } = require('pretzl');
 const { Bagel } = require('pretzl');
 const { batch } = require('pretzl');
 const { bag } = require('pretzl');
+const { toHaveBeenEatenMatcher } = require('../../bakery/lib/matchers/toHaveBeenCalled')
 
 
 let a = new Bagel('mock', ['method1', 'method2']);
+
+
+console.log("test has been eaten")
 
 batch("Bagel a", function() {
   bag("method 1 has been called", function() {
     a.method1();
     bite(a, "method1").toHaveBeenEaten()
-  });// Yuck
+  });
 });
-
-batch("Bagel a", function() {
-  bag("method 1 has not been called", function() {
-
-    bite(a, "method1").toHaveBeenEaten()
-  });// Yuck
-});
-
-
-//
-// a.method1();
-//
-// bite(a.method1).toHaveBeenEaten() // Yum
