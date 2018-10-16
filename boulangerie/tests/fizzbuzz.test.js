7ecce4c43d3226ac038f230372e329c39166a15f
Jon Urry <jon@urry.me>
2018-10-16 12:04:25 +0100

    colourise console

--- boulangerie/tests/fizzbuzz.test.js
+++ boulangerie/tests/fizzbuzz.test.js
@@ -1,4 +1,4 @@
-const fb = require("../lib/fizzbuzz.js")
+const fb = require('../lib/fizzbuzz.js');
 const { bite } = require('pretzl');
 
 bite(fb.fizzBuzz(1)).toEqual(1);
@@ -7,6 +7,7 @@ bite(fb.fizzBuzz(3)).toEqual('Fizz');
 bite(fb.fizzBuzz(4)).toEqual(4);
 bite(fb.fizzBuzz(5)).toEqual('Buzz');
 bite(fb.fizzBuzz(6)).toEqual('Fizz');
+bite(fb.fizzBuzz(7)).toEqual('error');
 bite(fb.fizzBuzz(7)).toEqual(7);
 bite(fb.fizzBuzz(8)).toEqual(8);
 bite(fb.fizzBuzz(9)).toEqual('Fizz');
