43283f535217d8184e42b6654897159b3ad63508
djstephenson7 <dan_0502@hotmail.co.uk>
2018-10-17 16:28:30 +0100

    created beforeEach and beforeAll functions and associated tests

--- boulangerie/tests/fizzbuzz.test.js
+++ boulangerie/tests/fizzbuzz.test.js
@@ -1,27 +1,46 @@
 const fb = require('../lib/fizzbuzz.js');
-const { bite } = require('pretzl');
-const { beforeEach } = require('pretzl');
-const { runEveryBeforeEach } = require('pretzl');
-const { beforeEachStack } = require('pretzl');
 
-beforeEach(function() {
-  console.log('Hi')
-})
+const { Bagel, bite, batch, bag, beforeEach, runEveryBeforeEach, beforeAll } = require('pretzl');
 
-runEveryBeforeEach(function() {
+batch("tests for fizzbuzz", () => {
 
-})
+  beforeAll(() => {
+  console.log('hey')
+  });
 
-bite(fb.fizzBuzz(1)).toTasteLike(1);
-bite(fb.fizzBuzz(2)).toTasteLike(2);
-bite(fb.fizzBuzz(3)).toTasteLike('Fizz');
-bite(fb.fizzBuzz(4)).toTasteLike(4);
-bite(fb.fizzBuzz(5)).toTasteLike('Buzz');
-bite(fb.fizzBuzz(6)).toTasteLike('Fizz');
-bite(fb.fizzBuzz(7)).toTasteLike('Buzz');
-bite(fb.fizzBuzz(7)).toTasteLike(7);
-bite(fb.fizzBuzz(8)).toTasteLike(8);
-bite(fb.fizzBuzz(9)).toTasteLike('Fizz');
-bite(fb.fizzBuzz(15)).toTasteLike('FizzBuzz');
-bite(fb.fizzBuzz(29)).toTasteLike(29);
-bite(fb.fizzBuzz(30)).toTasteLike('FizzBuzz');
+
+  bite(fb.fizzBuzz(1)).toTasteLike(1);
+  bite(fb.fizzBuzz(2)).toTasteLike(2);
+  bite(fb.fizzBuzz(3)).toTasteLike('Fizz');
+  bite(fb.fizzBuzz(4)).toTasteLike(4);
+  bite(fb.fizzBuzz(5)).toTasteLike('Buzz');
+  bite(fb.fizzBuzz(6)).toTasteLike('Fizz');
+  bite(fb.fizzBuzz(7)).toTasteLike('Buzz');
+  bite(fb.fizzBuzz(7)).toTasteLike(7);
+  bite(fb.fizzBuzz(8)).toTasteLike(8);
+  bite(fb.fizzBuzz(9)).toTasteLike('Fizz');
+  bite(fb.fizzBuzz(15)).toTasteLike('FizzBuzz');
+  bite(fb.fizzBuzz(29)).toTasteLike(29);
+  bite(fb.fizzBuzz(30)).toTasteLike('FizzBuzz');
+
+});
+
+
+
+  beforeEach(() => {
+  console.log('hey')
+  });
+
+  bite(fb.fizzBuzz(1)).toTasteLike(1);
+  bite(fb.fizzBuzz(2)).toTasteLike(2);
+  bite(fb.fizzBuzz(3)).toTasteLike('Fizz');
+  bite(fb.fizzBuzz(4)).toTasteLike(4);
+  bite(fb.fizzBuzz(5)).toTasteLike('Buzz');
+  bite(fb.fizzBuzz(6)).toTasteLike('Fizz');
+  bite(fb.fizzBuzz(7)).toTasteLike('Buzz');
+  bite(fb.fizzBuzz(7)).toTasteLike(7);
+  bite(fb.fizzBuzz(8)).toTasteLike(8);
+  bite(fb.fizzBuzz(9)).toTasteLike('Fizz');
+  bite(fb.fizzBuzz(15)).toTasteLike('FizzBuzz');
+  bite(fb.fizzBuzz(29)).toTasteLike(29);
+  bite(fb.fizzBuzz(30)).toTasteLike('FizzBuzz');
